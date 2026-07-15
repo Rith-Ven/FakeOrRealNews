2603edefe8f43ee882a993077fc6e404bdcd1fc3
Rith-Ven <rithanya.venkatesh16@gmail.com>
2026-07-15 18:21:10 -0400

    updated frontend

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pickle
+import newspaper
 
 st.set_page_config(page_title = "Fake News Detector", page_icon = "📰", layout = "centered")
 
-st.write("Enter an article or headline below to analyze its credibility.")
+st.write("Analyze news articles by pasting a URL link or the whole article below.")
 
 # Load in the saved model and vectorizer from train_and_save.py
 
@@ -24,18 +25,61 @@ except FileNotFoundError:
     st.stop()
 
 #Text input
-user_input = st.text_area("Enter News Article here:", height = 200, placeholder = "Type or paste news content...")
+#User_input = st.text_area("Enter News Article here:", height = 200, placeholder = "Type or paste news content...")
 
-#Predict button
-if st.button("Analyze Article"):
-    if user_input.strip() == "":
-        st.warning("Enter text before analyzing.")
+tab1, tab2 = st.tabs(["Analyze via URL", "Analyze via Text"])
+with tab1:
+    url_input = st.text_input("Paste News Article URL", placeholder = "https://www.example.com/new-article")
+    if st.button("Fetch & Analyze URL") == "":
+        st.warning("Please enter a valid URL.")
     else:
-        transformed_input = vectorizer.transform([user_input])
-        prediction = model.predict(transformed_input)[0]
-        st.write("---")
-        if prediction == "REAL":
-            st.success("This news looks REAL!")
+        with st.spinner("Fetching article content..."):
+            try:
+                article = newspaper.article(url_input)
+                article_text = article.text
+
+                if not article_text.strip():
+                    st.error("Could not extract any text from this URL.")
+                else:
+                    st.info(f"**Extracted Title:** {article.title}")
+                    with st.expander("Show scraped text snippet"):
+                        st.write(article_text[:600] + "...")
+                    transformed_input = vectorizer.transform([article_text])
+                    prediction = model.predict(transformed_input)[0]
+
+                    st.write("---")
+                    if prediction == "REAL":
+                        st.success("This news looks **REAL**")
+                    else:
+                        st.error("Warning: This news looks **FAKE**")
+
+            except Exception as e:
+                st.error(f"Error fetching article. Check the URL or try pasting the raw text. (Error: {e})")
+
+with tab2:
+    user_input = st.text_area("Paste news article text here: ", height=200, placeholder = "Type or paste text here...")
+
+    if st.button("Analyze raw text"):
+        if user_input.strip() == "":
+            st.warning("Please enter some text before analyzing.")
         else:
-            st.error("Careful! This news looks FAKE.")
+            transformed_input = vectorizer.transform([user_input])
+            prediction = model.predict(transformed_input)[0]
+            st.write("---")
+            if prediction == "REAL":
+                st.success("This news looks **REAL**")
+            else:
+                st.error("Warning: This news looks **FAKE**")
+#Predict button
+# if st.button("Analyze Article"):
+#     if user_input.strip() == "":
+#         st.warning("Enter text before analyzing.")
+#     else:
+#         transformed_input = vectorizer.transform([user_input])
+#         prediction = model.predict(transformed_input)[0]
+#         st.write("---")
+#         if prediction == "REAL":
+#             st.success("This news looks REAL!")
+#         else:
+#             st.error("Careful! This news looks FAKE.")
         
--- train_and_save.py
+++ train_and_save.py
@@ -2,24 +2,25 @@ import pandas as pd
 import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression # Updated model
-#from sklearn.linear_model import PassiveAggressiveClassifier
+#from sklearn.linear_model import LogisticRegression # Different model
+from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score
 
 df = pd.read_csv('news.csv')
 x_train, x_test, y_train, y_test = train_test_split(df['text'], df.label, test_size = 0.2, random_state = 7)
 
-tfidf_vectorizer = TfidfVectorizer(stop_words = 'english', max_df = 0.7, ngram_range = (1,2))
+# Captures 1 and 2 word phrases, ignores phrases appearing less than 3 times, keep only 25,000 most relevant features
+tfidf_vectorizer = TfidfVectorizer(stop_words = 'english', max_df = 0.7, ngram_range = (1,2), min_df = 3, max_features = 25000)
 tfidf_train = tfidf_vectorizer.fit_transform(x_train)
 tfidf_test = tfidf_vectorizer.transform(x_test)
 
-#pac = PassiveAggressiveClassifier(max_iter = 50, C=0.2)
-#pac.fit(tfidf_train, y_train)
+pac = PassiveAggressiveClassifier(max_iter=100, C=0.5, random_state=7)
+pac.fit(tfidf_train, y_train)
 
-lrModel = LogisticRegression(C = 2.0, max_iter = 1000)
-lrModel.fit(tfidf_train, y_train)
+#lrModel = LogisticRegression(C = 2.0, max_iter = 1000)
+#lrModel.fit(tfidf_train, y_train)
 
-predictions = lrModel.predict(tfidf_test)
+predictions = pac.predict(tfidf_test)
 new_score = accuracy_score(y_test, predictions)
 print(f"New Accuracy: {round(new_score*100,2)}")
 
@@ -27,6 +28,6 @@ with open('vectorizer.pkl', 'wb') as vec_file:
     pickle.dump(tfidf_vectorizer, vec_file)
 
 with open('model.pkl', 'wb') as model_file:
-     pickle.dump(lrModel,model_file)
+     pickle.dump(pac,model_file)
 
 print("save success")
